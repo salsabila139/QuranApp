4c139cbe0aeb5f8e2548f8eea297c6fbf8f74d3c
acer <salsabila1393@gmail.com>
2022-06-14 22:36:21 +0700

    Initial commit

---
+++ app/src/main/java/com/salsabila/idn/quranapp/SplashScreen.kt
@@ -0,0 +1,12 @@
+package com.salsabila.idn.quranapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.salsabila.idn.quranapp.R
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
