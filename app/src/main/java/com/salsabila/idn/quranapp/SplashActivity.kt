f3721a232e36f12b0206eb422a4e7c495b479402
salmalatifa12 <salmalathifah02@gmail.com>
2022-06-15 23:07:55 +0700

    finis splash screen

--- app/src/main/java/com/salsabila/idn/quranapp/SplashActivity.kt
+++ app/src/main/java/com/salsabila/idn/quranapp/SplashActivity.kt
@@ -1,11 +1,23 @@
 package com.salsabila.idn.quranapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()) //buat ngambil waktu
+            .postDelayed({
+                startActivity(Intent(this, LoginActivity::class.java))
+                finish() //buat matiin activity
+            }, SPLASH_TIME_OUT)
+    }
+    companion object{
+        private const val SPLASH_TIME_OUT = 3000L
     }
 }
